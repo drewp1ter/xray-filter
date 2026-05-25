1fea930fb8425933a7fe357095995f813e6a096b
drewp1ter <belora2010@yandex.ru>
2026-05-25 11:37:04 +0300

    update

--- src/app/lib/channel.py
+++ src/app/lib/channel.py
@@ -7,6 +7,8 @@ from app.lib.db import get_connection
 from app.lib.prometheus import fetch_uptime_stats
 from app.lib.constants import MAKE_POST_INTERVAL_SECONDS
 
+MAX_LATENCY_MS = 10000
+
 async def make_post():    
     online_proxies = [proxy for proxy in await get_validated_proxies() if proxy.online]
     print(f"Found {len(online_proxies)} online proxies.")
@@ -30,14 +32,16 @@ async def make_post():
     connection.close()
         
     for proxy in online_proxies:
+        if proxy.latencyMs > MAX_LATENCY_MS:
+            print(f"Skipping proxy {proxy.name} due to high latency: {proxy.latencyMs}ms")
+            continue
         decoded_url = base64.b64decode(proxy.originalData).decode('utf-8')
         print(decoded_url)
         created_at = next((row["created_at"] for row in created_at_list if row["stable_id"] == proxy.stableId), None)
         online_proxies_data.append({ "name": proxy.name, "decoded_url": decoded_url, "latency": proxy.latencyMs, "created_at": created_at, "uptime": uptime_stats.get(proxy.stableId, 100) })
     
-    # online_proxies_data = sorted(online_proxies_data, key=lambda p: (p["uptime"], -datetime.strptime(p["created_at"], "%Y-%m-%d %H:%M:%S").timestamp(), -p["latency"]), reverse=True)
-    
     if len(online_proxies_data) > 0:
+        online_proxies_data = sorted(online_proxies_data, key=lambda p: (p["uptime"], -datetime.strptime(p["created_at"], "%Y-%m-%d %H:%M:%S").timestamp(), -p["latency"]), reverse=True)
         message = "\n".join([f"**{p['latency']}ms** | `{p['name']}`\n`добавлен: {p['created_at']} | аптайм: {p['uptime'] if p['uptime'] > 0 else 100}%`\n```\n{p['decoded_url']}\n```\n" for p in online_proxies_data])
         await cleanup_telegram_messages()
         await send_telegram_message(message)
@@ -49,7 +53,7 @@ async def make_post():
 
 def can_make_new_post(cooldown_seconds: int) -> bool:
     now_hour = (datetime.now() + timedelta(hours=3)).hour
-    if now_hour < 6 or now_hour >= 22:
+    if now_hour < 6 or now_hour >= 23:
         return False
 
     age = get_last_sent_message_age_in_seconds()
