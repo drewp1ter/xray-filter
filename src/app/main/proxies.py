da9cd4cc2d0647395ac26d24564896d40ed844b7
drewp1ter <belora2010@yandex.ru>
2026-07-01 00:51:09 +0300

    update

--- src/app/main/proxies.py
+++ src/app/main/proxies.py
@@ -2,6 +2,7 @@ import os
 import re
 import asyncio
 import base64
+from datetime import datetime, timezone, timedelta
 from fastapi import APIRouter, Body, HTTPException, Query
 from fastapi.responses import PlainTextResponse
 from concurrent.futures import ThreadPoolExecutor, as_completed
@@ -12,7 +13,9 @@ import httpx
 
 router = APIRouter()
 
+UTC_PLUS_3 = timezone(timedelta(hours=3))
 last_wl_online_proxies: list[str] = []
+last_updated: datetime = datetime.now(UTC_PLUS_3)
 
 @router.get("/proxies", response_class=PlainTextResponse, )
 def get_proxies(
@@ -53,6 +56,8 @@ async def push_metrics(body: str = Body(..., media_type="text/plain")):
         raise HTTPException(status_code=500, detail=f"Failed to push metrics to Prometheus Pushgateway: {exc}")
     
     asyncio.create_task(make_post())
+    global last_updated
+    last_updated = datetime.now(UTC_PLUS_3)
     return {"status": "success", "message": "Metrics pushed successfully"}
 
 
@@ -60,13 +65,15 @@ async def push_metrics(body: str = Body(..., media_type="text/plain")):
 async def get_online_proxies():    
     wl_is_active = await get_wl_is_active()
     proxy_lines: list[str] = get_seen_online_proxies('1.5 hour')
-
-    sub = f"#profile-title: VPNClub | WL: {'ON' if wl_is_active else 'OFF'}\n" + \
-          "#profile-locked: false\n" + \
-          "#profile-update-interval: 1\n"
+    global last_updated
     global last_wl_online_proxies
     if wl_is_active and len(proxy_lines) > 0:
         last_wl_online_proxies = proxy_lines
+    sub = f"#profile-title: VPNBelora | WL: {'ON' if wl_is_active else 'OFF'}\n" + \
+        "#profile-locked: false\n" + \
+        f"#announce: Обновлено: {last_updated.strftime('%Y-%m-%d %H:%M')}, оператoр: t2\n" + \
+        "#profile-update-interval: 1\n" + \
+        "#providerid: dibtA9xQ\n"
     print(f"{len(proxy_lines)} online proxies, {len(last_wl_online_proxies)} is saved, WL is {'active' if wl_is_active else 'inactive'}")
     sub += "\n" + "\n".join(last_wl_online_proxies)
     return PlainTextResponse(content=base64.b64encode(sub.encode("utf-8")).decode(), media_type="text/plain")
